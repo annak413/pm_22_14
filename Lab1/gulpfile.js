73d4f3d4e821eac1a74c8e6c1bbf95e7c1a870ed
Anna Komar <anakomar586@gmail.com>
2025-09-10 23:34:26 +0300

    CommitGulp

--- Lab1/gulpfile.js
+++ Lab1/gulpfile.js
@@ -1,40 +1,71 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cssnano = require('gulp-cssnano');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
-
-// HTML таска
-function htmlTask() {
-   return src('./src/**/*.html')
-      .pipe(dest('./dist'));
-}
-exports.htmlTask = htmlTask;
-
-// SCSS таска
-function scssTask() {
-   return src('./src/**/*.scss')
-      .pipe(sass())
-      .pipe(cssnano())
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(dest('./dist/css'));
-}
-exports.scssTask = scssTask;
-
-// minimiz and copy
-function jsTask() {
-   return src('./src/**/*.js')
-      .pipe(uglify())//minimiz
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(dest('./dist/js'));
-}
-exports.jsTask = jsTask;
-
 const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
+
+function htmlTask() {
+    return src('./src/pages/**/*.html')
+        .pipe(dest('./dist'));
+}
+
+function scssTask() {
+    return src('./src/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(dest('./dist/css'))
+        .pipe(browserSync.stream()); 
+}
+
+function jsTask() {
+    return src('./src/js/**/*.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(dest('./dist/js'));
+}
 
 function imgTask() {
-   return src('./src/img/**/*.{jpg,jpeg,png,gif,svg}')
-      .pipe(imagemin())//optimization
-      .pipe(dest('./dist/img'));
+    return src('./src/img/**/*.{jpg,jpeg,png,gif,svg}')
+        .pipe(imagemin())
+        .pipe(dest('./dist/img'));
 }
+
+function serve() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+
+   // watch('./src/**/*.html').on('change', series(htmlTask, browserSync.reload));
+   // watch('./src/**/*.js').on('change', series(jsTask, browserSync.reload));
+   // watch('./src/**/*.scss', scssTask);
+   // watch('./src/img/**/*.{jpg,jpeg,png,gif,svg}').on('change', series(imgTask, browserSync.reload));
+
+
+   watch('./src/pages/**/*.html').on('change', series(htmlTask, function(done) {
+        browserSync.reload();
+        done();
+    }));
+
+    watch('./src/scss/**/*.scss', scssTask);
+
+    watch('./src/js/**/*.js').on('change', series(jsTask, function(done) {
+        browserSync.reload();
+        done();
+    }));
+
+    watch('./src/img/**/*.{jpg,jpeg,png,gif,svg}').on('change', series(imgTask, function(done) {
+        browserSync.reload();
+        done();
+    }));
+}
+
+exports.htmlTask = htmlTask;
+exports.scssTask = scssTask;
+exports.jsTask = jsTask;
 exports.imgTask = imgTask;
+exports.default = series(parallel(htmlTask, scssTask, jsTask, imgTask), serve);
