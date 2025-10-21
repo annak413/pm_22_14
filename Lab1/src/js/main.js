3e60073e4a835fe544dd4caa8f6feae92ae0d571
Anna Komar <anakomar586@gmail.com>
2025-10-21 23:32:32 +0300

    Array data

--- Lab1/src/js/main.js
+++ Lab1/src/js/main.js
@@ -28,4 +28,63 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
+
+  const educationData = [
+    {
+      degree: "MASTER",
+      specialization: "DESIGN WEB",
+      period: "Jan 2007 - Dic 2009",
+      institution: "University Of Lorem | Location",
+      description:
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam",
+    },
+    {
+      degree: "BACHELORS",
+      specialization: "OF ARTS",
+      period: "Mar 2004 - Feb 2007",
+      institution: "University Of Lorem | Location",
+      description:
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam .",
+    },
+  ];
+
+  function renderTimeline(data, selector) {
+    const container = document.querySelector(selector);
+
+    if (!container) return;
+
+    // 1. Очищення вмісту контейнера перед вставкою (як ви просили)
+    container.innerHTML = "";
+
+    // 2. Генерація розмітки, яка зберігає всі CSS-класи
+    const htmlMarkup = data
+      .map(
+        (item) => `
+                        <div class="inside_timeline">
+                          <div class="timeline-item row g-0">
+                            <div class="col-4">
+                              <h4 class="fs-6 fw-semibold text-uppercase lh-base mb-1">
+                                ${item.degree} <br />
+                                ${item.specialization}
+                              </h4>
+                              <p class="fs-6 lh-base fw-semibold">${item.period}</p>
+                            </div>
+                            <div class="col-8 left">
+                              <h5 class="fs-5 lh-base fw-semibold mb-0">
+                                ${item.institution}
+                              </h5>
+                              <p class="fs-6">
+                                ${item.description}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      `
+      )
+      .join("");
+
+    container.innerHTML = htmlMarkup;
+  }
+
+  renderTimeline(educationData, "#educationContent .timeline");
 });
