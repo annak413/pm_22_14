7caac5c094604b5c6380e23b30fc6ab147afc57f
Anna Komar <anakomar586@gmail.com>
2025-10-21 22:30:06 +0300

    Commit collapse

--- Lab1/src/js/main.js
+++ Lab1/src/js/main.js
@@ -7,4 +7,25 @@ document.addEventListener("DOMContentLoaded", () => {
   if (personName) {
     personName.innerHTML = `${firstName}<b class="fw-bold">${lastName}</b>`;
   }
+
+  const toggleButtons = document.querySelectorAll(".toggle-btn");
+
+  toggleButtons.forEach((button) => {
+    // 2. Прив'язуємо обробник події 'click'
+    button.addEventListener("click", function () {
+      // 3. Визначаємо цільовий контент (використовуючи data-target)
+      const targetId = this.getAttribute("data-target");
+      const content = document.getElementById(targetId);
+
+      if (content) {
+        // 4. Перемикаємо видимість контенту (додаємо/видаляємо службовий клас)
+        // Видимість (collapse-content active)
+        content.classList.toggle("active");
+
+        // 5. Перемикаємо клас на кнопці (для зміни орієнтації стрілки)
+        // Орієнтація стрілки (toggle-btn active)
+        this.classList.toggle("active");
+      }
+    });
+  });
 });
