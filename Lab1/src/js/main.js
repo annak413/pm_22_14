7f25fb15363dfefd93946fede0dee28407c35337
Anna Komar <anakomar586@gmail.com>
2025-10-27 18:13:04 +0200

    Last array commit

--- Lab1/src/js/main.js
+++ Lab1/src/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const firstName = "Laura";
+  const firstName = "Laura ";
   const lastName = "Anderson";
 
   const personName = document.querySelector(".person_name");
@@ -11,19 +11,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggleButtons = document.querySelectorAll(".toggle-btn");
 
   toggleButtons.forEach((button) => {
-    // 2. Прив'язуємо обробник події 'click'
     button.addEventListener("click", function () {
-      // 3. Визначаємо цільовий контент (використовуючи data-target)
       const targetId = this.getAttribute("data-target");
       const content = document.getElementById(targetId);
 
       if (content) {
-        // 4. Перемикаємо видимість контенту (додаємо/видаляємо службовий клас)
-        // Видимість (collapse-content active)
         content.classList.toggle("active");
 
-        // 5. Перемикаємо клас на кнопці (для зміни орієнтації стрілки)
-        // Орієнтація стрілки (toggle-btn active)
         this.classList.toggle("active");
       }
     });
